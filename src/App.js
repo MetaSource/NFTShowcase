385e2bb52af7cbd7fe1cf7f66cce0703e761925b
MetaSource <jsal91@gmail.com>
2023-01-09 02:21:06 -0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { Download, Features, SectionWrapper } from "./components";
+import assets from './assets'
+import styles from "./styles/Global";
 
-function App() {
+const App =()=> {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <SectionWrapper
+        title="Your own store of Nifty NFTs. 
+        Start Selling & Growing"
+        description="Buy, store, collect NFTs, 
+        exchange & earn crypto. 
+        Join 25+ million people using ProNef MarketPlace." 
+        showBtn 
+        mockupImg={assets.homeHero}
+        banner="banner"
+      />
+
+      <SectionWrapper
+        title="Smart User Interface Marketplace"
+        description="Experience a buttery UI of ProNef Marketplace. Smooth constant colors pf a fluent UI design. 
+        Join 25+ million people using ProNef MarketPlace." 
+        mockupImg={assets.homeCards}
+        reverse
+      />
+      <Features/>
+
+      <SectionWrapper
+        title="Deployment"
+        description="ProNef Is built using Expo which runs natively on all users' devices. You can easily get your app into people's hands" 
+        mockupImg={assets.feature}
+        reverse
+      />
+
+      <SectionWrapper
+        title="Creative way to showcase the store"
+        description="The app contains two screens, one to lists all the NFTs second to displays the details of a specific NFT." 
+        mockupImg={assets.mockup}
+        banner="banner02"
+        
+      />
+      <Download/>
+
+      <div className="px-4 py-2 justify-center items-center bg-primary flex-col text-center banner04">
+        <p className={`${styles.pText} ${styles.whiteText}`}>Made with love by {" "}
+        <span className="bold">Jorge Salcedo</span></p>
+      </div>
+    </>
   );
 }
 
